78405da2bcff5577192c01229ecf36bda126e933
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-07-19 17:40:32 +0300

    add Server delivery functionality

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+// open the database file
+const db = await open({
+    filename: "chat.db",
+    driver: sqlite3.Database,
+});
+
+// Create the messages table if it doesn't exist
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
 
 // Get the directory name of the current module
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -29,9 +46,40 @@ io.on("connection", async (socket) => {
     });
 
     // Listen for chat message events
-    socket.on("chat message", (msg) => {
+    socket.on("chat message", async (msg) => {
         console.log("message: " + msg);
+        // Save the message to the database
+        let result;
+        try {
+            result = await db.run(
+                "INSERT INTO messages (content) VALUES (?)",
+                msg
+            );
+        } catch (e) {
+            console.error("Error inserting message:", e);
+            return;
+        }
+        console.log("Message saved with ID:", result.lastID);
+
+        // Emit the message to all connected clients
+        io.emit("chat message", msg, result.lastID);
     });
+
+    if (!socket.recovered) {
+        // if the connection state recovery was not successful
+        try {
+            await db.each(
+                "SELECT id, content FROM messages WHERE id > ?",
+                [socket.handshake.auth.serverOffset || 0],
+                (_err, row) => {
+                    socket.emit("chat message", row.content, row.id);
+                }
+            );
+        } catch (e) {
+            // something went wrong
+            console.error("Error fetching messages:", e);
+        }
+    }
 
     // 
     try {
