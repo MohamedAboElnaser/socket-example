18d150bfbb81df5ddcb26461de9d971bb8ac9e90
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-07-19 12:36:27 +0300

    add index.html

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from "express";
 import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+// Get the directory name of the current module
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log("Current directory:", __dirname);
+
 
 //Create an Express application
 const app = express();
@@ -8,7 +16,7 @@ const app = express();
 const server = createServer(app);
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello world</h1>");
+    res.sendFile(join(__dirname, "index.html"));
 });
 
 server.listen(3000, () => {
